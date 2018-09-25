ffe407b04f6ebcf311090f325a80ef9105a540c1
user <user@es302n09>
2018-09-25 11:50:31 +0800

    initial

---
+++ Hello java/src/com/tom/Hello.java
@@ -0,0 +1,16 @@
+package com.tom;
+
+public class Hello {
+	public static void main(String[] args){	
+		int age ;
+		age = 19;
+		short number;
+		number = 8;
+		age = number;
+		float weight;
+		weight =(float)52.5;
+		System.out.println(number);
+		System.out.println(age);
+		System.out.println(weight);
+	}
+}
